f76fc4372e665fef893e331fc806b5f87a3a4113
Meryem Yanar <meryem.yanaryanar@gmail.com>
2017-05-14 15:22:48 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// SECTION ONE: Functions and objects definitions go here
+
+var questionsArray = [
+//Question 1
+    {
+        questionText: "Let's start off with an easy one. Which country's flag is this?",
+        questionImage: "uk.jpg"
+        questionChoices: ['England', 'France', 'The United Kingdom', 'Australia'],
+        questionCorrectChoice: 2,
+        correctDetails: "That's right, that's the flag of the United Kingdom! It includes England, Scotland, Wales, and Northern Ireland."
+		incorrectDetails: "That's the flag of the United Kingdom, which includes England, Scotland, Wales, and Northern Ireland."
+    },
+//Question 2
+	{
+		questionText: "Let's try another easy one. Which country's flag is this?",
+        questionImage: "uk.jpg"
+        questionChoices: ['India', 'East Timor', 'Germany', 'China'],
+        questionCorrectChoice: 3,
+        correctDetails: "You got it! That's the flag of the People's Republic of China."
+		incorrectDetails: "Whoops! That's actually the flag of the People's Republic of China."
+	},
+//Question 3
+	{
+		questionText: "Let's try another. Which country's flag is this?",
+        questionImage: "uk.jpg"
+        questionChoices: ['Barbados', 'Kenya', 'Zimbabwe', 'Malawi'],
+        questionCorrectChoice: 1,
+        correctDetails: "That's right! That's the flag of the Republic of Kenya, a country in eastern Africa."
+		incorrectDetails: "Sorry, that's not correct. That's the flag of the Republic of Kenya, a country in eastern Africa."
+	},
+//Question 4
+	{
+		questionText: "Can you identify this flag?",
+        questionImage: "uk.jpg"
+        questionChoices: ['Iran', 'Iraq', 'Syria', 'Italy'],
+        questionCorrectChoice: 0,
+        correctDetails: "Correct! That's the flag of the Islamic Republic of Iran, a country of almost 83 million people."
+		incorrectDetails: "Oops! That's actually the flag of the Islamic Republic of Iran, a country of almost 83 million people."
+	},
+//Question 5
+	{
+		questionText: "Can you identify the flag of this Central Asian country?",
+        questionImage: "uk.jpg"
+        questionChoices: ['Kazakhstan', 'Kyrgyzstan', 'Turkmenistan', 'Tajikistan'],
+        questionCorrectChoice: 2,
+        correctDetails: "You got it! That's the flag of Turkmenistan! The designs on the left side of the flag are five traditional patterns used in Turkmen carpets."
+		incorrectDetails: "Sorry, that's not it! It's the flag of Turkmenistan! The designs on the left side of the flag are five traditional patterns used in Turkmen carpets."
+	},
+//Question 6
+	{
+		questionText: "Which country's flag is this, featuring the flag of the United Kingdom as part of its design?",
+        questionImage: "uk.jpg"
+        questionChoices: ['Australia', 'New Zealand', 'Tuvalu', 'Heard and McDonald Islands'],
+        questionCorrectChoice: 2,
+        correctDetails: "Excellent! That's the flag of Tuvalu, an archipelago in the Pacific Ocean whose population is less than 10,000 people."
+		incorrectDetails: "Not exactly. That's the flag of Tuvalu, an archipelago in the Pacific Ocean whose population is less than 10,000 people."
+	},
+//Question 7
+	{
+		questionText: "Don't worry, there's just one more! Here's an easy one. Which country's flag is this?",
+        questionImage: "uk.jpg"
+        questionChoices: ['South Korea', 'Japan', 'Haiti', 'North Korea'],
+        questionCorrectChoice: 0,
+        correctDetails: "You got it! That's the flag of the Republic of Korea. The flag incorporates Taoist and Buddhist symbols."
+		incorrectDetails: "Sorry, that's not it! That's actually the flag of the Republic of Korea. The flag incorporates Taoist and Buddhist symbols."
+	}
+    ];
+
+// SECTION TWO: Triggers go here
+
+fucntion displayQuestions(currentQuestion) {
+	
+}
