51674feddca3bd27bb374154b1dad7a8546f5124
Meryem Yanar <meryem.yanaryanar@gmail.com>
2017-05-15 12:53:45 +0300

    Make styling pretty

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 // SECTION ONE: Functions and objects definitions go here
 
+// Objects
 var questionsArray = [
 //Question 1
     {
         questionText: "Let's start off with an easy one. Which country's flag is this?",
-        questionImage: "uk.jpg"
+        questionImage: "img/uk.png"
         questionChoices: ['England', 'France', 'The United Kingdom', 'Australia'],
         questionCorrectChoice: 2,
         correctDetails: "That's right, that's the flag of the United Kingdom! It includes England, Scotland, Wales, and Northern Ireland."
@@ -15,7 +16,7 @@ var questionsArray = [
 //Question 2
 	{
 		questionText: "Let's try another easy one. Which country's flag is this?",
-        questionImage: "uk.jpg"
+        questionImage: "img/china.png"
         questionChoices: ['India', 'East Timor', 'Germany', 'China'],
         questionCorrectChoice: 3,
         correctDetails: "You got it! That's the flag of the People's Republic of China."
@@ -24,7 +25,7 @@ var questionsArray = [
 //Question 3
 	{
 		questionText: "Let's try another. Which country's flag is this?",
-        questionImage: "uk.jpg"
+        questionImage: "img/kenya.png"
         questionChoices: ['Barbados', 'Kenya', 'Zimbabwe', 'Malawi'],
         questionCorrectChoice: 1,
         correctDetails: "That's right! That's the flag of the Republic of Kenya, a country in eastern Africa."
@@ -33,7 +34,7 @@ var questionsArray = [
 //Question 4
 	{
 		questionText: "Can you identify this flag?",
-        questionImage: "uk.jpg"
+        questionImage: "img/iran.png"
         questionChoices: ['Iran', 'Iraq', 'Syria', 'Italy'],
         questionCorrectChoice: 0,
         correctDetails: "Correct! That's the flag of the Islamic Republic of Iran, a country of almost 83 million people."
@@ -42,7 +43,7 @@ var questionsArray = [
 //Question 5
 	{
 		questionText: "Can you identify the flag of this Central Asian country?",
-        questionImage: "uk.jpg"
+        questionImage: "img/turkmenistan.png"
         questionChoices: ['Kazakhstan', 'Kyrgyzstan', 'Turkmenistan', 'Tajikistan'],
         questionCorrectChoice: 2,
         correctDetails: "You got it! That's the flag of Turkmenistan! The designs on the left side of the flag are five traditional patterns used in Turkmen carpets."
@@ -50,23 +51,56 @@ var questionsArray = [
 	},
 //Question 6
 	{
+		questionText: "Here's a challenge--which country's flag is this?",
+        questionImage: "img/vietnam.png"
+        questionChoices: ['Morocco', 'Vietnam', 'Kyrgyzstan', 'Macedonia'],
+        questionCorrectChoice: 1,
+        correctDetails: "Great work! The Socialist Republic of Vietnam is a communist state with 58 provinces and five municipalities."
+		incorrectDetails: "Uh-oh. That's actually the flag of the Socialist Republic of Vietnam, a communist state with 58 provinces and five municipalities."
+	},
+//Question 7
+	{
+		questionText: "Maybe something a bit easier? Which country's flag is this?",
+        questionImage: "img/france.png"
+        questionChoices: ['France', 'Russia', 'Luxembourg'],
+        questionCorrectChoice: 0,
+        correctDetails: "You got it! That's the flag of the French Republic."
+		incorrectDetails: "Non! That's the flag of the French Republic."
+	},
+//Question 8
+	{
 		questionText: "Which country's flag is this, featuring the flag of the United Kingdom as part of its design?",
-        questionImage: "uk.jpg"
+        questionImage: "img/tuvalu.png"
         questionChoices: ['Australia', 'New Zealand', 'Tuvalu', 'Heard and McDonald Islands'],
         questionCorrectChoice: 2,
         correctDetails: "Excellent! That's the flag of Tuvalu, an archipelago in the Pacific Ocean whose population is less than 10,000 people."
 		incorrectDetails: "Not exactly. That's the flag of Tuvalu, an archipelago in the Pacific Ocean whose population is less than 10,000 people."
 	},
-//Question 7
+//Question 9
+	{
+		questionText: "Which country has this pretty red flag?",
+        questionImage: "img/turkey.png"
+        questionChoices: ['Hong Kong', 'Pakistan', 'Tunisia', 'Turkey'],
+        questionCorrectChoice: 3,
+        correctDetails: "Aferin! (That's Turkish for \"great job\"!) The modern Republic of Turkey was founded in 1923 by Mustafa Kemal Ataturk."
+		incorrectDetails: "Oh no! That's the flag of the Republic of Turkey, founded in 1923 by Mustafa Kemal Ataturk."
+	},
+//Question 10
 	{
 		questionText: "Don't worry, there's just one more! Here's an easy one. Which country's flag is this?",
-        questionImage: "uk.jpg"
+        questionImage: "img/south-korea.png"
         questionChoices: ['South Korea', 'Japan', 'Haiti', 'North Korea'],
         questionCorrectChoice: 0,
         correctDetails: "You got it! That's the flag of the Republic of Korea. The flag incorporates Taoist and Buddhist symbols."
 		incorrectDetails: "Sorry, that's not it! That's actually the flag of the Republic of Korea. The flag incorporates Taoist and Buddhist symbols."
 	}
     ];
+
+// Functions
+
+// Step 1: Load the page; show only the first section (#start-page)
+
+// Step 2: 
 
 // SECTION TWO: Triggers go here
 
